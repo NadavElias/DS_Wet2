21a8be0a5a4b03fb725905670703976d72f37d99
Nadav Elias <nadavelias@gmail.com>
2018-12-30 18:19:22 +0200

    היי שגיא

---
+++ HTable.h
@@ -0,0 +1,8 @@
+//
+// Created by nadav.elias on 30-Dec-18.
+//
+
+#ifndef DS_WET2_HTABLE_H
+#define DS_WET2_HTABLE_H
+
+#endif //DS_WET2_HTABLE_H
---
+++ RTree.h
@@ -0,0 +1,8 @@
+//
+// Created by nadav.elias on 30-Dec-18.
+//
+
+#ifndef DS_WET2_RTREE_H
+#define DS_WET2_RTREE_H
+
+#endif //DS_WET2_RTREE_H
